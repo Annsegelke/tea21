02416323f6471d4d09541d5f42590ae809f43c94
Annsegelke <149794272+Annsegelke@users.noreply.github.com>
2023-12-01 15:17:48 +0100

    Änderungen Übung 4

--- exercise-004/main.cpp
+++ exercise-004/main.cpp
@@ -3,9 +3,12 @@
 
 #include "CLI/CLI.hpp"
 #include "config.h"
+#include "bmp.h"
+
 
 auto main(int argc, char **argv) -> int
 {
+    std::string filename{DEFAULT_BMP_FILE};
     /**
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
@@ -14,6 +17,7 @@ auto main(int argc, char **argv) -> int
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        app.add_option("-f,--file",filename,fmt::format("The filename for the Bitmap, deault : {}", filename));
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
@@ -27,8 +31,12 @@ auto main(int argc, char **argv) -> int
      * More info at https://fmt.dev/latest/api.html
      */
     fmt::print("Hello, {}!\n", app.get_name());
+    fmt::println("The filename to use: {}", filename);
 
     /* INSERT YOUR CODE HERE */
+
+    BMP bmp;
+    
 
     return 0; /* exit gracefully*/
 }
